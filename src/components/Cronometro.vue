3360622de9b5f0ce55889ce569a38e70d69914ba
Psychovv <viniciusmedina63@gmail.com>
2026-05-13 13:50:07 -0300

    refactor: refatorando a pagina Formulairo.vue criando um component temporizador que passa a abrigar a logica antes no proprio Formulario.vue

--- src/components/Cronometro.vue
+++ src/components/Cronometro.vue
@@ -2,7 +2,7 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'TempoDecorrido',
+    name: 'CronometroFix',
     props: {
         tempoEmSegundos: {
             type: Number,
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import CronometroFix from "./Cronometro.vue";
+
+export default defineComponent({
+  name: "TemporizadorFix",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0
+    }
+  },
+  methods: {
+    iniciar() {
+      // começar a contagem
+      // 1 seg = 1000 ms
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000)
+    },
+    finalizar() {
+      clearInterval(this.cronometro)
+    }
+  },
+  components: { CronometroFix } 
+});
+</script>
+
+
+<template>
+<div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+      </div>
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <CronometroFix :tempoEmSegundos="tempoEmSegundos" />
+          <button class="button" @click="iniciar">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button" @click="finalizar">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+</template>
+
+<style scoped>
+</style>
